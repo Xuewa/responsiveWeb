17e48e3c5f4c9ec01f7f9399a82f04dd16115201
Xuewa <366653992@qq.com>
2016-11-13 19:48:54 +0800

    add owl carousel plugin

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 /**
  * Created by Administrator on 2016/11/8 0008.
  */
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true
+    });
+});
